0ca74e3105e20ffd1d32ea196f48e1256e974ffb
panticaleksandar <46134708+panticaleksandar@users.noreply.github.com>
2023-04-10 07:51:27 +0200

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route exact path="/" element={<Layout />}>
+        <Route exact path="/portfolio" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
