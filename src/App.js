339f2abbfed21dcdb2bbc4fa5a5f045e2d266983
panticaleksandar <46134708+panticaleksandar@users.noreply.github.com>
2023-04-10 07:30:17 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.scss'
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
@@ -9,13 +9,13 @@ import Contact from "./components/Contact/Contact";
 const App = () => {
   return (
     <>
-    <Routes>
+    <BrowserRouter basename='/portfolio'>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Route>
-    </Routes>
+    </BrowserRouter>
     </>
   )
 }
