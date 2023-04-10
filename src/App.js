3388bf790abc5f73c3c4f97c7105820f2f29bbe8
panticaleksandar <46134708+panticaleksandar@users.noreply.github.com>
2023-04-10 07:50:38 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React from 'react'
 import './App.scss'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout/Layout";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
 
 const App = () => {
   return (
     <>
-    <Routes >
-      <Route exact path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route exact path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
     </>
   )
 }
 
-export default App;
+export default App
